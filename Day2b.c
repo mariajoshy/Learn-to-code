baf0e584d3ff2262c499cbd3b4a7b98ec1b6ffed
mariajoshy <97902251+mariajoshy@users.noreply.github.com>
2022-05-09 19:25:50 +0530

    Create Day2b.c
    Remainder

---
+++ Day2b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int a=7,b=3,c=10,d=9;
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    scanf("%d\n",&c);
+    scanf("%d",&d);
+    printf("%d",a%b+c%d);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
