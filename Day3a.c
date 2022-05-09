ef01a6606b1609472ec64d3cdc1ac99a3567a207
mariajoshy <97902251+mariajoshy@users.noreply.github.com>
2022-05-09 19:35:01 +0530

    Create Day3a.c
    Volume

---
+++ Day3a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    float l=4.2,b=7.4,h=9.1;
+    scanf("%f\n",&l);
+    scanf("%f\n",&b);
+    scanf("%f\n",&h);
+    printf("%0.3f\n",l*l*l);
+    printf("%0.3f\n",l*b*h);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+
