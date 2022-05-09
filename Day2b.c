a433d128ab9354119d92b75aaea867b700bc80e0
mariajoshy <97902251+mariajoshy@users.noreply.github.com>
2022-05-09 19:10:47 +0530

    Create Day2b.c
    Variables

---
+++ Day2b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+    int a=1;
+    scanf("%d\n",&a);
+    char w;
+    scanf("%c\n",&w);
+    float b=2.5;
+    scanf("%f",&b);
+    printf("%d\n",a);
+    printf("%c\n",w);
+    printf("%f",b);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+
