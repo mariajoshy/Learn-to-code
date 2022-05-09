4808d0cb26abb1f63e4a13b08c79fb1f566467a6
mariajoshy <97902251+mariajoshy@users.noreply.github.com>
2022-05-09 19:05:36 +0530

    Create Day1a.c
    To print

---
+++ Day1a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    printf("Hello\tWorld\n");
+    printf("Welcome to Learn to code 2022");
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+
+    
