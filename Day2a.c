9d4c06df2493e545a671754096fee7398b849523
mariajoshy <97902251+mariajoshy@users.noreply.github.com>
2022-05-09 19:13:56 +0530

    Create Day2a.c
    Simple calculator

---
+++ Day2a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int a=113,b=7;
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%.2f\n",(float)a/b);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+
