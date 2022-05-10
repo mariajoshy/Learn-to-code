f91e3139283381188da787b6bf64f9644a237354
mariajoshy <97902251+mariajoshy@users.noreply.github.com>
2022-05-10 21:14:32 +0530

    Create Day4b.c

---
+++ Day4b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+    int a;
+    scanf("%d",&a);
+    if (a%2==0)
+{
+        printf("Even\n");
+        printf("%d\n",a*a);
+}
+    else
+    
+    {
+        printf("Odd\n");
+        printf("%d\n",a*a*a);
+    }
+        
+    
+ 
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
